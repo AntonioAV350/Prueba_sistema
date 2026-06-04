31da72a1b2eb52988d1a72118174164f09e80a05
Yaelcruz05-git <yaelcruzcalvo06@gmail.com>
2026-06-04 11:50:17 -0600

    Actualización del frontend

---
+++ Frontend/src/views/AdminView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="dashboard-panel">
+    <h1>Hola Admin</h1>
+    <p>Sesión activa con privilegios de Administrador del Sistema.</p>
+    <router-link to="/">Finalizar Sesión</router-link>
+  </div>
+</template>
---
+++ Frontend/src/views/UserView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="dashboard-panel">
+    <h1>Hola Usuario</h1>
+    <p>Sesión activa con privilegios de Usuario Común.</p>
+    <router-link to="/">Finalizar Sesión</router-link>
+  </div>
+</template>
